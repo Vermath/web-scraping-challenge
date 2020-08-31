4b1e5168037b68a6eef2c98089a3008b19619dcf
Vermath <65049377+Vermath@users.noreply.github.com>
2020-08-31 16:32:31 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Flask instance
+app = Flask(__name__)
+
+
+# Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Tell it what index to take
+@app.route("/")
+def index():
+
+    # Where to look in mongo
+    mars_dict = mongo.db.mars_dict.find_one()
+    # Template + data
+    return render_template("index.html", mars=mars_dict)
+
+
+@app.route("/scrape")
+def scrape():
+  
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
